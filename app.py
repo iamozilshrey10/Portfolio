701482ab0bdcab3ec778df4b59cf7fdac20f1cbb
ShreyashSartandel <sartandelshreyash2@gmail.com>
2020-08-22 18:04:18 -0400

    2nd Upgrade

--- app.py
+++ app.py
@@ -11,21 +11,24 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+"""
 def write_to_file(data):
 	with open('web_server/database.txt', mode='a') as database:
 		email = data["email"]
 		subject = data["subject"]
 		message = data["message"]
 		file = database.write(f'\n{email},{subject},{message}')
+"""
 
 def write_to_csv(data):
-	with open('web_server/database.csv', newline='', mode='a') as database2:
-		email = data["email"]
-		subject = data["subject"]
-		message = data["message"]
-		csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		csv_writer.writerow([email,subject,message])
-
+		with open("C:\\Users\\sarta\\Desktop\\Portfolio\\database.csv", newline='', mode='a') as database2:
+			
+			email = data["email"]
+			subject = data["subject"]
+			message = data["message"]
+			csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			csv_writer.writerow([email,subject,message])
+	
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
